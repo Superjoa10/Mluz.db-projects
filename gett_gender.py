d238041c52aee92e929e378e40823a8400beaaa8
João Luz <joaovictorluz@gmail.com>
2022-09-04 22:35:37 -0300

    Update gett_gender.py

--- gett_gender.py
+++ gett_gender.py
@@ -1,4 +1,5 @@
-import gender_guesser.detector as gender, pyautogui as gui
+import gender_guesser.detector as gender, pyautogui as gui, re, time
+
 def get_gender(prompt):
     L = gender.Detector()
     result = (L.get_gender(prompt))
@@ -25,3 +26,92 @@ def get_gender(prompt):
             return ["senhor", "do", "o"]
         elif real_result == "senhora":
             return ["senhora", "da", "a"]
+
+def who_acd(obs):
+        acd_dev = re.compile(r"(acd_dev|Acd_Dev|acd_Dev|Acd_dev")
+        acd_form = re.compile(r"(acd_form|Acd_Form|acd_Form|Acd_form")
+        matches_acd = acd_dev.findall(obs)
+        for match_acd in matches_acd:
+                    if  not match_acd:
+                        matches_acd_form = acd_form.findall(obs)
+                        for match_acd_forms in matches_acd_form:
+                            if not match_acd_forms:
+                                Raise: TypeError("Lol sem def de acordo")
+                            return False
+                    return True
+
+def dev_num(obs,forms):
+    if forms == True:
+        n_form = re.compile(r"(N_form:|n_form) (\([1-9]{2}\)|[1-9]{2}) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4})| S/nada)")
+        n_dev = re.compile(r"(N_dev:|n_dev) (\([1-9]{2}\)|[1-9]{2}) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4})| S/nada)")
+        whom_acd = who_acd(obs)
+        #true is devedor
+        if whom_acd == True:
+            matches_dev = n_dev.findall(obs)
+        for match_dev in matches_dev:
+            if not match:
+                print (" Sem num devedor!!")
+                pass
+            print(f"numero dev {match_dev[1]}")
+            num_dev = match_dev[1]
+            return num_dev
+            
+        if whom_acd == False:
+            matches_form = n_form.findall(obs)
+        for match_form in matches_form:
+            if not match:
+                print (" Sem num formando")
+                pass
+            print(f"n formando {match_form[1]}")
+            num_forms = match_form[1]
+            return num_forms
+        global numbers_geral
+        numbers_geral = [num_dev, num_forms]
+
+    elif forms == False:
+        n_unico = re.compile(r"(N:|n:) ((\([1-9]{2}\)|[1-9]{2}) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4})| S/nada)")
+        matches_Dev = n_unico.findall(obs)
+        for match in matches_Dev:
+            if not match:
+                print("U GAY AS FUCK")
+                pass
+            print(f"dev unico :{match}")
+            num = match_form[1]
+            return num
+
+def form(obs):
+    form = re.compile(r"(Formando:|formando:|FORMANDO:) (o mesmo|O MESMO|O Mesmo|o Mesmo|O mesmo|[aA-zZ*\_\-\s]+\n)")
+    matches = form.findall(obs)
+    #match is a list of ["formando" , "O mesmo"]
+    for match in matches:
+        return match[1]
+
+def a_form(formando):
+    if formando == "o mesmo":
+        return False
+    elif formando == "O MESMO":
+        return False
+    elif formando == "O Mesmo":
+        return False
+    elif formando == "o Mesmo":
+        return False
+    elif formando == "O mesmo":
+        return False
+    elif formando == None:
+        return False
+    elif formando <= len(8): 
+        return True
+
+def get_time():
+    currentTime  = int(time.strftime('%H')) 
+    if currentTime < 12 :
+        return('Bom dia')
+    if currentTime > 12 :
+        return('Boa tarde')
+    if currentTime > 6 :
+        return('Boa noite')
+
+def main():
+    pass
+if __name__ == "__main__":
+    main()
