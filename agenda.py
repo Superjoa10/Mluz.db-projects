0f7e072ad519833d399805017e2885bf8a54ef3f
João Luz <joaovictorluz@gmail.com>
2022-11-12 19:25:07 -0300

    Update agenda.py

--- agenda.py
+++ agenda.py
@@ -66,7 +66,9 @@ def comp(nome):
 def main(navegador):
         with open("acordos.csv", "r", encoding="Latin-1") as file:  
             csv_reader = csv.reader(file, delimiter=';')  
-            line_count = 0  
+            line_count = 0 
+            acordos_cob = []
+            acords_n_cob = []
             for lol in csv_reader:
                 if line_count == 0:  
                     line_count += 1   
@@ -82,65 +84,86 @@ def main(navegador):
                         if cobr == "sim":
                             numero = comp(nome)  
                             if numero == None:
-                                if obs == "0":
-                                   obs == None 
-                                print("----------------------------------------------------------------------------------")
-                                print(f"no num on case {nome}")
-                                gui.alert(text=f'''O caso {nome} não possui numero!
-                                Obs: {obs}''', title='Aviso', button='Pass')
-                                pass
+                                        if obs == "0":
+                                            obs == None 
+                                        print("----------------------------------------------------------------------------------")
+                                        print(f"no num on case {nome}")
+                                        gui.alert(text=f'''O caso {nome} não possui numero!
+                                        Obs: {obs}''', title='Aviso', button='Pass')
+                                        acords_n_cob.append(nome)
+                                
                             else:
                                 if forms == True:
                                     if who_acd(obs_dev) == True:
                                         print("----------------------------------------------------------------------------------")
                                         print(f"Cobrando acordo do {nome}, acordo sendo com o devedor {numero}, porem possui formando")
-                                        cobrar(nome, dia_atual, numero, navegador)  
+                                        cobrar(nome, dia_atual, numero, navegador)
+                                        acordos_cob.append(nome)
                                         print(f"{nome} cobrado(a)")  
                                     elif who_acd(obs_dev) == False:
                                         print("----------------------------------------------------------------------------------")
                                         print(f"Cobrando acordo do {nome}, acordo sendo com o formando: {formando} {numero}")
-                                        cobrar(formando, dia_atual, numero, navegador)  
+                                        cobrar(formando, dia_atual, numero, navegador)
+                                        acordos_cob.append(formando)  
                                         print(f"{nome} cobrado(a)") 
                                 else:
-                                    print("----------------------------------------------------------------------------------")
-                                    print(f"Cobrando acordo do {nome}, acordo sendo com o devedor {numero}")
-                                    cobrar(nome, dia_atual, numero, navegador)  
-                                    print(f"{nome} cobrado(a)")  
+                                        print("----------------------------------------------------------------------------------")
+                                        print(f"Cobrando acordo do {nome}, acordo sendo com o devedor {numero}")
+                                        cobrar(nome, dia_atual, numero, navegador)
+                                        acordos_cob.append(nome)  
+                                        print(f"{nome} cobrado(a)")  
                         elif cobr == "nao":
-                            if obs == "0":
-                                obs == None
-                            print("----------------------------------------------------------------------------------")  
-                            gui.alert(text=f'''O caso {nome} esta com cobrança automatica desligada!
-                            Obs: {obs}''', title='Aviso', button='OK')
+                                        if obs == "0":
+                                            obs == None
+                                        print("----------------------------------------------------------------------------------")  
+                                        gui.alert(text=f'''O caso {nome} esta com cobrança automatica desligada!
+                                        Obs: {obs}''', title='Aviso', button='OK')
+                                        acords_n_cob.append(nome)
 
                     if dia_atual == prazo: 
                         if cobr == "sim":
                             numero = comp(nome) 
                             if numero == None:
-                                print("----------------------------------------------------------------------------------")
-                                gui.alert(text=f'''O caso {nome} não possui numero!
-                                Obs: {obs}''', title='Aviso', button='Pass')
-                                print(f"No num in case {nome}")
-                                pass
+                                        print("----------------------------------------------------------------------------------")
+                                        gui.alert(text=f'''O caso {nome} não possui numero!
+                                        Obs: {obs}''', title='Aviso', button='Pass')
+                                        print(f"No num in case {nome}")
+                                        acords_n_cob.append(nome)
                             else:
                                 numero = comp(nome)  
                                 if forms == True:
                                     if who_acd(obs_dev) == True:
                                         print("----------------------------------------------------------------------------------")
                                         print(f"Cobrando acordo do {nome}, acordo sendo com o devedor {numero}, porem possui formando")
-                                        cob_prazo(nome, dia_atual, numero, navegador)  
+                                        cob_prazo(nome, dia_atual, numero, navegador)
+                                        acordos_cob.append(nome)   
                                         print(f"{nome} cobrado(a)")  
                                     elif who_acd(obs_dev) == False:
                                         print(f"Cobrando acordo do {nome}, acordo sendo com o formando: {formando} {numero}")
-                                        cob_prazo(formando, dia_atual, numero, navegador)  
+                                        cob_prazo(formando, dia_atual, numero, navegador)
+                                        acordos_cob.append(formando)   
                                         print(f"{nome} cobrado(a)") 
                                 else:
-                                    print("----------------------------------------------------------------------------------")
-                                    print(f"Cobrando acordo do {nome}, acordo sendo com o devedor {numero}")
-                                    cob_prazo(nome, dia_atual, numero, navegador)  
-                                    print(f"{nome} cobrado(a)")  
+                                        print("----------------------------------------------------------------------------------")
+                                        print(f"Cobrando acordo do {nome}, acordo sendo com o devedor {numero}")
+                                        cob_prazo(nome, dia_atual, numero, navegador)
+                                        acordos_cob.append(nome) 
+                                        print(f"{nome} cobrado(a)")
+
+                        elif cobr == "nao":
+                                        if obs == "0":
+                                            obs == None
+                                        print("----------------------------------------------------------------------------------")  
+                                        gui.alert(text=f'''O caso {nome} esta com cobrança automatica desligada!
+                                        Obs: {obs}''', title='Aviso', button='OK')
+                                        acords_n_cob.append(nome)
+
                     line_count += 1  
-            print(f'hoje é dia {dia_atual}. Acordos ativos: {line_count - 1}. ')
+            print(f'''hoje é dia {dia_atual}. 
+Acordos ativos: {line_count - 1}. 
+acordos cobrados hoje: {len(acordos_cob)}
+lista acordos cob. automatica desligada: {acords_n_cob}''')
+            gui.alert(text=f'''Todos acordos possiveis cobrados do dia {dia_atual}''', title='Done', button='OK')
 
 if __name__ == "__main__":
     navegador = webdriver.Chrome()
